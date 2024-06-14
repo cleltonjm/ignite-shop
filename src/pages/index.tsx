cd8456b90087246c9c6d6e67633f6112792a35db
Clelton Mielke <cleltonjm_95@hotmail.com>
2024-06-13 21:08:22 -0300

    Utilizando SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import Image from "next/image";
-
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
 import { useKeenSlider } from 'keen-slider/react'
-
+import { GetStaticProps } from "next";
 import { HomeContainer, Product } from "../styles/pages/home";
 
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
-
 import 'keen-slider/keen-slider.min.css'
-import { stripe } from "../lib/stripe";
-import { GetServerSideProps } from "next";
-import Stripe from "stripe";
 
 interface HomeProps {
   products: {
@@ -48,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -60,13 +54,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100),
     }
   })
 
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60  * 2, // 2 hours
   }
 }
