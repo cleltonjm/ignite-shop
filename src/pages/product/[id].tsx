076f5d51cff60a187f7773bc403dc921546a0a38
Clelton Mielke <cleltonjm_95@hotmail.com>
2024-07-24 21:13:31 -0300

    Botão de comprar

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -3,6 +3,8 @@ import { GetStaticPaths, GetStaticProps } from "next"
 import { stripe } from "../../lib/stripe"
 import Stripe from "stripe"
 import Image from "next/image"
+import axios from "axios"
+import { useState } from "react"
 
 interface ProductProps {
     product: {
@@ -16,6 +18,28 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+    async function handleBuyProduct() {
+        try {
+            setIsCreatingCheckoutSession(true);
+
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId,
+            })
+
+            const { checkoutUrl } = response.data;
+
+            window.location.href = checkoutUrl
+        } catch (err) {
+            // Conectar com uma ferramenta de observabildiade (Datadog / Sentry)
+
+            setIsCreatingCheckoutSession(false);
+
+            alert('Falha ao redirecionar ao checkout!')
+        }
+    }
+
     return (
         <ProductContainer>
             <ImageContainer>
@@ -27,7 +51,7 @@ export default function Product({ product }: ProductProps) {
                 <span>{product.price}</span>
 
                 <p>{product.description}</p>
-                <button>
+                <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
                     Comprar agora
                 </button>
             </ProductDetails>
